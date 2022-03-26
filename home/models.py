77136c303e1a012020c1c617174c7477fe3ebbb8
vitormiura <vitormiura3@gmail.com>
2022-03-26 09:33:35 -0300

    att models from home

--- home/models.py
+++ home/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Aluno(models.Model):
+    curso = models.Charfield(max_length=30)
+    descricao = models.Charfield(max_length=100)
+    status = models.Charfield(
+        max_length=100,
+        choices=(
+            ('A', 'Ativo'),
+            ('B', 'Bloqueado'),
+            ('C', 'Cancelado'),
+        )
+    )
+
+    def __str__(self):
+        return self.nome
+
+
+class Curso(models.Model):
+    curso = models.Charfield(max_length=30)
+    descricao = models.Charfield(max_length=100)
+    status = models.Charfield(
+        max_length=100,
+        choices=(
+            ('A', 'Ativo'),
+            ('I', 'Inativo'),
+        )
+    )
+
+    def __str__(self):
+        return self.Curso
